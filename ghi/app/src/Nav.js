49c82ca1437125b377121bbb4f0279245ec83c9f
Vincent Manfre <x333coins@gmail.com>
2023-03-06 17:53:00 -0800

    front + nav update

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -5,7 +5,6 @@ function Nav() {
     <nav className="navbar navbar-expand-lg navbar-dark bg-success">
       <div className="container-fluid">
         <NavLink className="navbar-brand" to="/">CarCar</NavLink>
-        <NavLink className="nav-link" to="/models">Models</NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
