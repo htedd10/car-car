0627925dc89024ce2f402d00da4db2adb381dd96
Ted <htedd10@gmail.com>
2023-03-08 20:21:32 -0800

    Change vehicle models list to have manufacturer filters and fix create vehicle model form from not populating manufacturers after submitting

--- ghi/app/src/Inventory/VehicleModelsForm.js
+++ ghi/app/src/Inventory/VehicleModelsForm.js
@@ -43,7 +43,6 @@ function CreateVehicleModelForm(props) {
                 setName('');
                 setPicture('');
                 setManufacturer('');
-                setManufacturers([]);
             }
 
 
